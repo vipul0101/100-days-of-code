f2cdcc7e5caf374f4133f330d5f33d919f1823f9
vipul <vipulgupta8383015057@gmail.com>
2023-02-22 15:41:41 +0530

    this is day 35

---
+++ day 35.py
@@ -0,0 +1,17 @@
+## here we learn about for loop with else statement
+for i in range(5):
+    print(i)
+
+else:
+    print("sorry no i")
+
+
+#### special case ####
+
+for i in range(6):
+    print(i)
+    if(i==4):
+        break
+
+else:
+    print("sorry no i")
