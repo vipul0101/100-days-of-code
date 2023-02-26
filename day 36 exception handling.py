83e6b3291042d187eb16d1624d495db5568b6838
vipul <vipulgupta8383015057@gmail.com>
2023-02-26 20:05:45 +0530

    this file is completed

--- day 36 exception handling.py
+++ day 36 exception handling.py
@@ -5,3 +5,19 @@ print(f"Multiplication table of {a} is: ")
 
 for i in range(1,11):
     print(f"{int(a)} X {i} = {int(a)*i}")
+
+# but if I give input as a string then it will give error 
+# and now for handling the error we use some process like this shown below
+
+a = input("enter the number :")
+print(f"Multiplication table of {a} is: ")
+
+try:
+    for i in range(1,11):
+        print(f"{int(a)} X {i} = {int(a)*i}")
+
+except Exception as e:
+    print("this is invalid input error")
+
+print('your code gives an error')
+print("please change your lines")
