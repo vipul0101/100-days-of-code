e9e736f41ebeafb36eefddd359c186516d91e8d6
vipul <vipulgupta8383015057@gmail.com>
2023-03-01 10:43:48 +0530

    1 more modification is done

--- day 39 exercise 3.py
+++ day 39 exercise 3.py
@@ -48,7 +48,8 @@ levels = [1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000, 320000, 64
 money=0
 for i in range(0, len(questions)):
     question = questions[i]
-    print(f"\n\nquestion for rupees {levels[i]}")
+    print(f"\n\nquestion for rupees {levels[i]} is: ")
+    print(f"{question[0]}")
 
     print(f"a.{question[1]}     b.{question[2]}")
     print(f"c.{question[3]}     d.{question[4]}")
