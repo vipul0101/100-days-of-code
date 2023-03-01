4fe26e763681639c172cedbb47c10cad6179152b
vipul <vipulgupta8383015057@gmail.com>
2023-03-01 10:39:07 +0530

    i have modified day 39 file

--- day 39 exercise 3.py
+++ day 39 exercise 3.py
@@ -53,7 +53,11 @@ for i in range(0, len(questions)):
     print(f"a.{question[1]}     b.{question[2]}")
     print(f"c.{question[3]}     d.{question[4]}")
 
-    reply=int(input("enter your answer (1-4)"))
+    reply=int(input("enter your answer (1-4) or 0 to quit:\n"))
+    if(reply == 0):
+        money = levels[i-1]
+        break
+        
     if(reply == question[-1]):
         print(f"correct answer, you have won Rs.{levels[i]}")
         if(i==4):
