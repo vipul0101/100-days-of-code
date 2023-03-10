457fdb1d1cfc01b75675847575108ae6cc8a91a7
vipul <vipulgupta8383015057@gmail.com>
2023-03-10 16:47:44 +0530

    100 days of code continues

---
+++ day37.py
@@ -0,0 +1,5 @@
+# HERE WE LEARN ABOUT FINALLY KEYWORD
+from harry import welcome, harry
+
+welcome()
+print(harry)
---
+++ harry.py
@@ -0,0 +1,9 @@
+def welcome():
+    print("hey you are welcome my friend")
+
+harry = "you are a good boy"
+
+import os
+
+print(os.getcwd())
+print(os.listdir(100 DAYS OF CODE=''))
